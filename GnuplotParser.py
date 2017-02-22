226ac24eb7b6281ed7d7dadec991424b5b5780ec
Nikhil Vittal Shetty <nshetty@localhost.localdomain>
2017-02-22 09:13:35 +0100

    the package can now plot 1D data, and multiple plots

--- GnuplotParser.py
+++ GnuplotParser.py
@@ -6,7 +6,9 @@ It has dependency on the GnuplotDataStructure package.
 
 import numpy as np
 import matplotlib.pyplot as plt
-from GnuplotDataStructure import GnuplotDataStructure as GDS
+#from GnuplotDataStructure import GnuplotDataStructure as GDS
+import shlex # to preserve substrings in one piece while splitting using space
+from StringIO import StringIO # to pack list into raw data, so as to be easily read by np.loadtxt()
 
 
 class GnuplotParser:
@@ -21,43 +23,111 @@ class GnuplotParser:
         '__init__' -- print activation information
 
     """
+
+    filename = ''
     
     def __init__(self):
         print('GnuplotParser objects can be created')
 
+    def parseGnuplotComamnd(self, gnuplot_cmd):
+
+        # set up axes
+        set_dict = {}
+        for item in gnuplot_cmd.splitlines():
+            if "set" in item.split():
+
+                item = item.split()
+
+                if len(item) != 3: # ignore if keyword is empty
+                    continue
+
+                if 'logscale' in item: # keyword logscale is repeated
+                    if 'y' in item:
+                        plt.yscale('log')
+                    elif 'x' in item:
+                        plt.xscale('log')
+
+                set_dict[item[1]] = item[2] # fill dictionary
+
+        print set_dict
+
+        if 'title' in set_dict:
+            plt.title(set_dict['title'].replace("'", ""))
+
+        if 'xlabel' in set_dict:
+            plt.xlabel(set_dict['xlabel'].replace("'", ""))
+
+        if 'ylabel' in set_dict:
+            plt.ylabel(set_dict['ylabel'].replace("'", ""))
+
+        if 'xrange' in set_dict:
+            xlimit_list = (set_dict['xrange'].replace("[", "").replace("]", "")).split(":")
+            if xlimit_list[0]:
+                plt.xlim(left = float(xlimit_list[0]))
+            if xlimit_list[1]:
+                plt.xlim(right = float(xlimit_list[1]))
+
+        if 'yrange' in set_dict:
+            ylimit_list = (set_dict['yrange'].replace("[", "").replace("]", "")).split(":")
+            if ylimit_list[0]:
+                plt.ylim(top = float(ylimit_list[0]))
+            if ylimit_list[1]:
+                plt.ylim(bottom = float(ylimit_list[1]))
+
+
+
+
+        # set up plot options
+        superplotstring = gnuplot_cmd.splitlines()[-1]  # the last line of the Gnuplot command
+        plotstring_list = superplotstring.split(",")
+        print plotstring_list
+
+        for plotstring in plotstring_list:
+            self.parsePlotString(plotstring)
+
+        plt.legend()
+        plt.show()
+
+
     def parsePlotString(self, plotstring):
-        print(plotstring)
-        #filename = plotstring.split()[1].replace("'", "") # extract second word and stip apostrophes
-        #index = int(plotstring.split()[3]) # extract index and cast to integer
-        #
-        #span = 2
-        #plot_dict = [tuple(words[i:i + span]) for i in range(0, len(words), span)]
-        #
-        words = plotstring.split()
-        plot_dict = dict(map(None, *[iter(words)] * 2))
+
+        global filename
+
+        words = shlex.split(plotstring) # to preserve substrings in one piece while splitting using space
+        if words[0] == 'plot' or words[0] == 'splot':
+            plot_dict = dict(map(None, *[iter(words)] * 2))
+            filename = plot_dict["plot"].replace("'", "")
+        elif words[0] == '':
+            plot_dict = dict(map(None, *[iter(words[1:])] * 2))
+        else:
+            filename = words[0]
+            plot_dict = dict(map(None, *[iter(words[1:])] * 2))
         print plot_dict
         #
-        filename = plot_dict["plot"].replace("'", "")
+
         index = int(plot_dict["ind"])
 
         using = plot_dict["us"].split(":")
         x = int(using[0])-1
         y = int(using[1])-1
 
-        block = 0
+        if 'every' in plot_dict:
+            block = int(plot_dict['every'].split(":")[-1])
+        else:
+            block = -1 # that is there is no block info
 
-        print filename, index, x, y
+        print filename, index, x, y, block
 
-        #GDS().plotData(filename, index, block=0, x=int(using[0])-1, y=int(using[1])-1)
-        #def plotData(self, filename, index, block, x, y):
-        data = GDS().parseDatafile(filename, index, block)
+        data = self.parseDatafile(filename, index, block)
 
-        # check for title
-        if 't' in plot_dict:
-            plt.title(plot_dict['t'].replace("'", ""))
+
+
 
         # key word arguments
         kwargs = {}
+
+        if 't' in plot_dict: #legend
+            kwargs['label'] = plot_dict['t'].replace("'", "")
 
         if 'w' in plot_dict:
             kwargs['linestyle'] = 'steps'
@@ -67,6 +137,24 @@ class GnuplotParser:
 
 
         plt.errorbar(x=data[:, x], y=data[:, y], **kwargs)
-        plt.show()
 
         
+
+
+
+
+
+    def parseDatafile(self, filename, index, block):
+        file_handle = open(filename, 'r')
+        raw_data = file_handle.read()
+        file_handle.close()
+
+        dataset = raw_data.split('\n\n\n')
+
+      	if block != -1:
+            datablock = dataset[index].split('\n\n')
+            raw_datablock = StringIO(datablock[block])
+	else:
+            raw_datablock = StringIO(dataset[index])
+
+        return np.loadtxt(raw_datablock)
